298287401ec370186f41e9b8baa156ae10952b6e
Žan Žiber <zan.zibert@ulu.io>
2019-10-30 11:04:59 +0100

    implemented chech if vehicle in db function

--- bmw_umbrella_umbrella/apps/bmw_umbrella/lib/bmw_umbrella/business_logic.ex
+++ bmw_umbrella_umbrella/apps/bmw_umbrella/lib/bmw_umbrella/business_logic.ex
@@ -1,5 +1,7 @@
 defmodule BmwUmbrella.BusinessLogic do
   alias BmwUmbrella.Auth
+  alias BmwUmbrella.Capabilities
+  alias BmwUmbrella.ClientApi
 
   def valid_token?(token) do
     case Auth.get_token_by_value!(token) do
@@ -26,6 +28,20 @@ defmodule BmwUmbrella.BusinessLogic do
 
       true ->
         true
+    end
+  end
+
+  def check_if_vehicle_in_db(vin) do
+    case Capabilities.get_vehicle_by_vin!(vin) do
+      nil ->
+        {:ok, capabilities} = ClientApi.check_if_vehicle_is_bmw_cardata_capable(vin) 
+        vehicle = [vin: vin] ++ for {key, value} <- capabilities, do: {String.to_atom(Macro.underscore(key)), value}
+
+        Enum.into(vehicle, %{})
+        |> Capabilities.create_vehicle()
+
+      vehicle ->
+        IO.inspect "trololo"
     end
   end
 end
